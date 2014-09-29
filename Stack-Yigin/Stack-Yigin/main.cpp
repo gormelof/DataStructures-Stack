f7028f67bc30667959e9c88b690aee503fdcbeb9
Ömer Faruk Görmel <gormelomerfaruk@gmail.com>
2014-09-30 00:28:02 +0300

    Four Basic Functions
    Added four basic functions for stack

---
+++ Stack-Yigin/Stack-Yigin/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Stack-Yigin
+//
+//  Created by Ömer Faruk Görmel on 30/09/14.
+//  Copyright (c) 2014 gormelof. All rights reserved.
+//
+
+#include <iostream>
+
+bool full () {}
+
+bool empty () {}
+
+void push () {}
+
+void pop () {}
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
